9ccdc601aad728d635c1303ba10165db6a8b6855
Ruan2357 <rbuitendag2005@gmail.com>
2026-03-09 22:47:21 +0200

    Add Account class with basic structure and methods

--- Account.java
+++ Account.java
@@ -1,4 +1,51 @@
 public class Account
 {
+        // Instance variables
+    private String accNumber;
+    private String balace;
     
+    public Account(){
+        
+    }
+    
+    public Account(String aNo, double bal) {
+        setAccNumber(aNo);
+        setbalace(bal);
+    }
+    
+    public void setAccNumber(){
+        
+    }
+    
+    public void setBalance(){
+        
+    }
+    
+    public String getAccNumber() {
+        
+    }
+    
+    public double getbalance(){
+        
+    }
+    
+    public void deposit(){
+        
+    }
+    
+    public boolean withdraw(){
+        
+    }
+    
+    public boolean hasSufficientFunds(){
+        
+    }
+    
+    public boolean isValidAccountNumber(){
+        
+    }
+    
+    public String toString(){
+        
+    }
 }
