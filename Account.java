7856ed97c433f9390c60d0caf2f56afe4345c52f
Ruan2357 <rbuitendag2005@gmail.com>
2026-03-09 23:04:28 +0200

    Fix balance variable and update related methods

--- Account.java
+++ Account.java
@@ -2,44 +2,45 @@ public class Account
 {
         // Instance variables
     private String accNumber;
-    private String balace;
+    private double balance;
     
     public Account(){
-        
+        accNumber = "";
+        balance = 0.0;
     }
     
     public Account(String aNo, double bal) {
         setAccNumber(aNo);
-        setbalace(bal);
+        setBalance(bal);
     }
     
-    public void setAccNumber(){
-        
+    public void setAccNumber(String aNo){
+        accNumber = aNo;
     }
     
-    public void setBalance(){
-        
+    public void setBalance(double bal){
+        balance = bal;
     }
     
     public String getAccNumber() {
-        
+        return accNumber;
     }
     
     public double getbalance(){
-        
+        return balance;
     }
     
-    public void deposit(){
-        
+    public void deposit(double amount){
+        balance += amount;
     }
     
-    public boolean withdraw(){
-        
+    public boolean withdraw(double amount){
+
     }
     
-    public boolean hasSufficientFunds(){
-        
-    }
+    public boolean hasSufficientFunds(double amount){
+
+        }
     
     public boolean isValidAccountNumber(){
         
