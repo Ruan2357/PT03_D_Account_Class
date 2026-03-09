5e04bf89e97be208bfc8fdaabee5dc55f9fe2e37
Ruan2357 <rbuitendag2005@gmail.com>
2026-03-09 23:45:02 +0200

    Account class methods and validations
    Went ahead and completed the last few methods. really had to spend sometime with the isValidAccountNumber() method, but it checks uppercase, '-' as the 4th char and that the last 6 chars are digits. Also made sure to include String.format("%.2f", balance) to make my output a bit cleaner

--- Account.java
+++ Account.java
@@ -1,6 +1,5 @@
 public class Account
 {
-        // Instance variables
     private String accNumber;
     private double balance;
     
@@ -35,18 +34,44 @@ public class Account
     }
     
     public boolean withdraw(double amount){
-
+        if (hasSufficientFunds(amount)){
+            balance -= amount;
+            return true;
+        }
+        return false;
     }
     
     public boolean hasSufficientFunds(double amount){
-
+        return balance >= amount;
         }
     
     public boolean isValidAccountNumber(){
+        if (accNumber == null || accNumber.length() != 10){
+            return false;
+        }
         
+        // First 3 must be uppercase;
+        for (int i = 0; i < 3; i++){
+            char ch = accNumber.charAt(i);
+            if (ch < 'A' || ch > 'Z'){
+                return false;
+            }
+        }
+        // checks if 4th char is '-'
+        if (accNumber.charAt(3) != '-'){
+            return false;
+        }
+        // checks if last 6 chars are digits
+        for (int i = 4; i < 10; i++){
+            char ch = accNumber.charAt(i);
+            if (ch <'0' || ch > '9'){
+                return false;
+            }
+        }
+        return true;
     }
     
     public String toString(){
-        
+        return "Account Number: " + accNumber + "\nBalance: R" + String.format("%.2f", balance);
     }
 }
